34d9074780cee5cad183f9dfb6d49ef14da4c793
Patrick Poulain <petitchevalroux@free.fr>
2018-11-09 16:32:25 +0100

    Add https certificats to haproxy

--- docker/haproxy/entrypoint.sh
+++ docker/haproxy/entrypoint.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+set -e
+
 /etc/init.d/rsyslog start
+
+cd /etc/haproxy/certs/
+openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365 -subj "/C=FR/ST=IDF/L=Paris/O=Baguette Corp/OU=Org/CN=docker.localhost" -nodes
+cat key.pem cert.pem > docker.localhost.pem
+
 /usr/sbin/haproxy -f /etc/haproxy/haproxy.cfg -db
